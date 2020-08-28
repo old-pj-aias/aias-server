1550a57868edf771087ea4640832d737a931b8e6
kino-ma <kino.ma.ms@gmail.com>
2020-08-29 04:24:24 +0900

    [update] Some comments

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# We must generate executable manually to link Rust lib, not `go run`.
+# We must generate executable manually to link Rust lib. (Not `go run`.)
 go build -ldflags="-r ./core" src/main.go
 ./main
