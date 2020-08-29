e68ecb66f185c181434e9b476aeec8fc3f84f591
kino-ma <kino.ma.ms@gmail.com>
2020-08-30 01:38:07 +0900

    [add] Communications with aias-core

--- app/src/main.go
+++ app/src/main.go
@@ -2,15 +2,18 @@ package main
 
 /*
 #cgo LDFLAGS: -L../core -laias_core
+#include <stdlib.h>
 #include "../core/core.h"
 */
 import "C"
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
-	"math/big"
 	"net/http"
+	"os"
+	"unsafe"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -33,6 +36,7 @@ import (
 }
 */
 
+/*
 const (
 	p = big.NewInt(241)
 	q = big.NewInt(719)
@@ -86,18 +90,66 @@ type EncryptedMessage struct {
 type Unblinder struct {
 	R []big.Int `json:"r"`
 }
+*/
 
 // SampleServer sends "Hello world" response to any incoming requests
 func SampleServer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "Hello world")
 }
 
+// Check checks validity of sent parameters
+func Check(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "failed to read body")
+		return
+	}
+
+	bodyC := C.CString(string(body))
+	valid := C.check(bodyC)
+
+	if valid == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid")
+	} else {
+		fmt.Fprintf(w, "ok")
+	}
+}
+
 func main() {
 	log.Println("starting server...")
 
-	router := httprouter.New()
+	pubkey := readFile("keys/id_rsa.pub")
+	pubkeyC := C.CString(string(pubkey))
 
+	privkey := readFile("keys/id_rsa")
+	privkeyC := C.CString(string(privkey))
+
+	C.new(privkeyC, pubkeyC)
+	defer C.destroy()
+
+	C.free(unsafe.Pointer(pubkeyC))
+	C.free(unsafe.Pointer(privkeyC))
+
+	router := httprouter.New()
 	router.GET("/", SampleServer)
+	router.POST("/check", Check)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func readFile(file string) []byte {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal("failed to open", file)
+	}
+	defer f.Close()
+
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal("failed to read", file)
+	}
+
+	return content
 }
