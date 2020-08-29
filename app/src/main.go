63c664882fd7d254e7f6c5ffff8eb169894c65a9
kino-ma <kino.ma.ms@gmail.com>
2020-08-30 08:04:20 +0900

    [add] API to start signing protocol

--- app/src/main.go
+++ app/src/main.go
@@ -1,9 +1,9 @@
 package main
 
 /*
-#cgo LDFLAGS: -L../core -laias_core
+#cgo LDFLAGS: -L../aias-core/go -laias_go
 #include <stdlib.h>
-#include "../core/core.h"
+#include "../aias-core/go/aias_go.h"
 */
 import "C"
 
@@ -18,83 +18,35 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-/*
-{
-    "part_of_encrypted_message": {
-        "u": [
-            "vector of string"
-        ]
-    },
-    "part_of_unblinder": {
-        "r": [
-            "vector of int"
-        ]
-    },
-    "part_of_beta": [
-        "vector of byte"
-    ]
-}
-*/
-
-/*
-const (
-	p = big.NewInt(241)
-	q = big.NewInt(719)
-	n = p.Mul(q)
-	e = 23
-	k = 30
-
-	// ID is session identifier
-	ID = "hoge"
-	// S is subset of [1..2k]
-	S = []int{1, 4, 7, 8, 10, 11, 12, 14, 17, 18, 22, 23, 24, 26, 30}
+var (
+	privkeyC     *C.char
+	pubkeyC      *C.char
+	judgePubKeyC *C.char
 )
 
-// EJ is Judge's encryption function
-func EJ(m big.Int) (big.Int, error) {
-	res := m.Exp(2, nil).Add(1)
-
-	err := nil
-	if res == nil {
-		err = "failed"
-	}
-
-	return res, err
-}
-
-// H is one way hash function
-func H(m big.Int) (big.Int, error) {
-	res := m.Add(1).Exp(2, nil)
-
-	err := nil
-	if res == nil {
-		err = "failed"
-	}
-
-	return res, err
-}
-
-// CheckParameter is parameters sent from Sender
-type CheckParameter struct {
-	PartOfEncryptedMessage EncryptedMessage `json:"part_of_encrypted_message"`
-	PartOfUnblinder        Unblinder        `json:"part_of_unblinder"`
-	PartOfBeta             []bytes          `json:"part_of_beta"`
-}
-
-// EncryptedMessage is m_i where i belongs to S
-type EncryptedMessage struct {
-	U []string `json:"u"`
-}
-
-// Unblinder is r_i where i belongs to S
-type Unblinder struct {
-	R []big.Int `json:"r"`
-}
-*/
-
-// SampleServer sends "Hello world" response to any incoming requests
+// SampleServer sends "Hello world" response for requests to /
 func SampleServer(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "Hello world")
+}
+
+// Start starts communication with Sender
+func Start(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "failed to read body")
+		return
+	}
+
+	bodyC := C.CString(string(body))
+	C.new(privkeyC, pubkeyC, judgePubKeyC)
+
+	C.set_blinded_digest(bodyC)
+
+	subset := C.setup_subset()
+	subsetGo := C.GoString(subset)
+
+	fmt.Fprintf(w, subsetGo)
 }
 
 // Check checks validity of sent parameters
@@ -121,19 +73,23 @@ func main() {
 	log.Println("starting server...")
 
 	pubkey := readFile("keys/id_rsa.pub")
-	pubkeyC := C.CString(string(pubkey))
+	pubkeyC = C.CString(string(pubkey))
 
 	privkey := readFile("keys/id_rsa")
-	privkeyC := C.CString(string(privkey))
+	privkeyC = C.CString(string(privkey))
 
-	C.new(privkeyC, pubkeyC)
+	judgePubKey := readFile("keys/judge_id_rsa.pub")
+	judgePubKeyC = C.CString(string(judgePubKey))
+
 	defer C.destroy()
 
-	C.free(unsafe.Pointer(pubkeyC))
-	C.free(unsafe.Pointer(privkeyC))
+	defer C.free(unsafe.Pointer(pubkeyC))
+	defer C.free(unsafe.Pointer(privkeyC))
+	defer C.free(unsafe.Pointer(judgePubKeyC))
 
 	router := httprouter.New()
 	router.GET("/", SampleServer)
+	router.POST("/start", Start)
 	router.POST("/check", Check)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
