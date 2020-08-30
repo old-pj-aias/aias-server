4c40c47ebe3f8c1b3a7e51aaecd0c06d0a3799e9
akakou <akakou571@gmail.com>
2020-08-31 03:11:55 +0900

    [add] Create table

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use rusqlite::{params, Connection, Result};
+use rusqlite::NO_PARAMS;
+
+use std::fs;
 
 async fn hello() -> impl Responder {
     println!("request");
@@ -8,6 +12,19 @@ async fn hello() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    fs::remove_file("db.sqlite3");
+    let conn = Connection::open("db.sqlite3").unwrap();
+
+    conn.execute(
+        "CREATE TABLE sign_process (
+                  id              INTEGER PRIMARY KEY,
+                  phone           TEXT NOT NULL,
+                  m               TEXT NOT NULL,
+                  subset          TEXT NOT NULL
+                  )",
+        params![],
+    ).unwrap();
+
     println!("server started");
 
     HttpServer::new(|| {
