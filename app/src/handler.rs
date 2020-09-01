e6d5be0337da7234fda1cacc8a9e4f1d624b1362
kino-ma <kino.ma.ms@gmail.com>
2020-09-01 23:01:28 +0900

    [add] Function to create table

--- app/src/handler.rs
+++ app/src/handler.rs
@@ -1,7 +1,7 @@
 use actix_web::{HttpResponse, Responder, web};
 use fair_blind_signature::CheckParameter;
 
-use aias_core::signer::Signer;
+use aias_core::signer::{Signer, ReadyParams};
 use std::sync::{Arc, Mutex};
 
 use crate::utils::{self, Keys};
@@ -16,19 +16,24 @@ pub async fn hello() -> impl Responder {
 pub async fn ready(body: web::Bytes, actix_data: web::Data<Arc<Mutex<Keys>>>) -> Result<String, HttpResponse> {
     println!("ready");
 
+    let id = 10;
+
     let signer_privkey = actix_data.lock().unwrap().signer_privkey.clone();
     let signer_pubkey = actix_data.lock().unwrap().signer_pubkey.clone();
 
     let body = String::from_utf8_lossy(&body).to_string();
-    let mut signer = Signer::new_with_blinded_digest(signer_privkey, signer_pubkey, body);
+    let mut signer = Signer::new_with_blinded_digest(signer_privkey, signer_pubkey, body.clone(), id);
+
+    let digest_and_ej = serde_json::from_str(&body).expect("failed to parse json");
+    let ReadyParams { judge_pubkey, blinded_digest } = digest_and_ej;
 
     let subset: String = signer.setup_subset();
 
     let conn = utils::db_connection();
 
-    conn.execute("INSERT INTO sign_process (phone, m, subset)
-                  VALUES ($1, $2, $3)",
-                 &["10", &body.to_string(), &subset]).unwrap();
+    conn.execute("INSERT INTO sign_process (phone, m, subset, session_id, judge_pubkey)
+                  VALUES ($1, $2, $3, $4, $5)",
+                 &["10", &body.to_string(), &subset, &"10".to_string(), &judge_pubkey]).unwrap();
 
     Ok(subset)
 }
@@ -43,22 +48,20 @@ pub async fn sign(body: web::Bytes, actix_data: web::Data<Arc<Mutex<Keys>>>) -> 
     let signer_pubkey = actix_data.lock().unwrap().signer_pubkey.clone();
 
     let conn = utils::db_connection();
-    let mut stmt = conn.prepare("SELECT m, subset, session_id, judge_pubkey FROM sign_process WHERE phone=?")
+    let mut stmt = conn.prepare("SELECT m, subset, judge_pubkey FROM sign_process WHERE session_id=?")
         .expect("failed to select");
 
     struct SignData {
         m: String,
         subset: String,
-        id: u32,
         judge_pubkey: String
     }
 
-    let SignData {m, subset, id, judge_pubkey} = stmt.query_row(rusqlite::params!["10"], |row| {
+    let SignData {m, subset, judge_pubkey} = stmt.query_row(rusqlite::params![id], |row| {
         Ok(SignData {
             m: row.get(0).unwrap(),
             subset: row.get(1).unwrap(),
-            id: row.get(2).unwrap(),
-            judge_pubkey: row.get(3).unwrap()
+            judge_pubkey: row.get(2).unwrap()
         })
     })
     .unwrap();
